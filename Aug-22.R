5fe2f9fa59fd0ba5c4588ac9b18a459ae0eecd87
Amrinder Singh <117053349+AmrinderGlLL@users.noreply.github.com>
2024-08-22 16:25:30 -0400

    Add files via upload

---
+++ Aug-22.R
@@ -0,0 +1,18 @@
+#Author:Amrinder Singh, Date: 08/22/2024, Purpose: Test the measures of descriptive stats
+
+#How to calculate standard deviation in R
+
+x <- c(1,2,3,4,5)
+
+y <- 100
+
+sd(x)
+
+#How to calculate mean in R
+
+mean(x)
+
+#How to calculate median in R
+
+median(x)
+
