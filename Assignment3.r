524bb3c2b302fc4d34daceabdcfa21f0d1e532b1
Amrinder Singh <117053349+AmrinderGlLL@users.noreply.github.com>
2024-08-29 16:19:02 -0400

    Add files via upload

---
+++ Assignment3.r
@@ -0,0 +1,17 @@
+#Author:Amrinder Singh, Date: 08/29/2024, Purpose: In class assignment.
+
+
+#Creating the variable_1 and populating it with random numbers
+ 
+variable_1 <- runif(n = 1000, min = 1, max = 99999999999)
+
+
+mean_val <- mean(variable_1) 
+median_val <- median(variable_1)
+sd_val <- sd(variable_1)
+
+#mean = 50339008738
+
+#median = 49668828840
+
+#sd = 28303596972
