7351c4bdea8d0b0597ce786cd9ff887a9f29039b
Amrinder Singh <117053349+AmrinderGlLL@users.noreply.github.com>
2024-09-17 16:02:58 -0400

    Add files via upload

---
+++ Assignment-4.R
@@ -0,0 +1,10 @@
+#Author:Amrinder Singh, Date: 9/17/2024, Purpose: In class assignment to calculate correlation value.
+
+my_data <- mtcars
+
+answer <- cor(my_data$wt, my_data$gear)
+
+
+#The correlation value is:  -0.583287
+ 
+
